5eafb51e9d372d347024d1d50978d58fb57792ca
Eduardo Valenzuela <evalenzuela@aurasix.com>
2018-10-18 09:49:17 -0500

    Initial Component Structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileFormComponent } from './profile/profile-form/profile-form.component';
+import { ProfileDetailComponent } from './profile/profile-detail/profile-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProfileComponent,
+    ProfileFormComponent,
+    ProfileDetailComponent
   ],
   imports: [
     BrowserModule
